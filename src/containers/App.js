4ba9d637508ca2ca46d9d4c81d8432849a19d58d
AndyKovv <andy.kovv@gmail.com>
2016-06-03 14:00:40 +0300

    Simple React-Router Structure

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,39 +1,20 @@
 import React, { Component } from 'react'
-import Admin from '..//components/Admin'
-import Genre from '..//components/Genre'
-import Home from '..//components/Home'
+import { Link } from 'react-router'
 
 export default class App extends Component {
-	constructor (props){
-		super(props)
-		this.state = {
-			route: window.location.hash.substr(1)
-		}
-	}
-	componentDidMount(){
-		window.addEventListener('hashchange', () => { //Событие hashchange при изменении мы вызываем изменнение state (Подписались на событие)
-			this.setState({
-				route: window.location.hash.substr(1) //Изменяем занчение  route
-			})
-		})
-	}
-	render() {
-		let Child //Создаем внутреннюю переменную окружения
 
-		switch (this.state.route) {
-			case '/admin': Child = Admin; break; //Сравниваем обрезаный URL с шаблоном и присваеваем компоненте Child значение
-			case '/genre': Child = Genre; break;
-			default: Child = Home; //Если ни один шаблон не подошел утанавливает стандартный шаблон.
-			// В этом примере мы присваиваем переменной Child компоненты в зависимости от установленого URL и рендерим его.
-		}
+	render() {
+		
 		return (
 			<div className='container'>
 				<h1>APP</h1>
 				<ul>
-					<li><a href='#/admin'>Admin</a></li>
-					<li><a href='#/genre'>Genre</a></li>
+					<li><Link to='/admin'>Admin</Link></li>
+					<li><Link to='/genre'>Genre</Link></li>
 				</ul>
-				<Child />
+
+				{/* вывод потомков, тоесть выводим содержимое компонетов Admin/Genre */}
+				{ this.props.children}
 			</div>
 		) 
 	}
--- src/index.js
+++ src/index.js
@@ -2,9 +2,20 @@ import 'babel-polyfill'
 import React from 'react'
 import { render } from 'react-dom'
 import App from './containers/App'
+import Admin from './components/Admin'
+import Genre from './components/Genre'
+import Home from './components/Home'
+
+import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 
 
 render(
-	<App />,
+	<Router history={browserHistory}>
+		<Route path='/' component={App}>
+			<IndexRoute component={Home} /> {/* Задаёться роут для корня тоесть для / */}
+			<Route path='admin' component={Admin} /> {/* Вложили admin в Route / чтобы компоненты были доступны по URL /admin */}
+			<Route path='genre' component={Genre} />
+		</Route>
+	</Router>,
 	document.getElementById('root')
 )
